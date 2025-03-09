f0527248099fe01051380826d3075c41795f20c4
chaewon-huh <cw.huh@postech.ac.kr>
2025-03-09 10:59:13 +0900

    chore : change training paramters

--- src/train/train_dlora_flux_advanced.sh
+++ src/train/train_dlora_flux_advanced.sh
@@ -1,15 +1,12 @@
 export MODEL_NAME="black-forest-labs/FLUX.1-dev"
 export INSTANCE_DIR="./data/dreambooth/instance"
-export CLASS_DIR="./data/dreambooth/class"
-export OUTPUT_DIR="./ckpt/refinement-v0.0.2-flux-advanced"
+export OUTPUT_DIR="./ckpt/ponix-generator"
 
 poetry run accelerate launch train/train_dlora_flux_advanced.py \
   --pretrained_model_name_or_path "$MODEL_NAME" \
   --instance_data_dir "$INSTANCE_DIR" \
-  --class_data_dir=$CLASS_DIR \
   --output_dir "$OUTPUT_DIR" \
   --mixed_precision "bf16" \
-  --with_prior_preservation \
   --num_class_images 1000 \
   --resolution 768 \
   --train_batch_size 1 \
@@ -20,10 +17,9 @@ poetry run accelerate launch train/train_dlora_flux_advanced.py \
   --lr_warmup_steps 0 \
   --max_train_steps 2000 \
   --gradient_checkpointing \
-  --token_abstraction "hcwf" \
-  --instance_prompt "3d ultrasound image of hcwf baby face, headshot" \
-  --class_prompt "3d ultrasound image of baby face, headshot" \
-  --initializer_concept "3d ultrasound image of baby face, headshot" \
+  --token_abstraction "ponix" \
+  --instance_prompt "photo of ponix plush bird" \
+  --initializer_concept "photo of red plush bird" \
   --train_transformer_frac 1 \
   --train_text_encoder_ti \
   --train_text_encoder_ti_frac 0.5 \
@@ -32,3 +28,4 @@ poetry run accelerate launch train/train_dlora_flux_advanced.py \
   --push_to_hub \
   --resume_from_checkpoint="latest" \
   --cache_dir="/workspace/ods-refinement-dev/model" \
+  --center_crop
