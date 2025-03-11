80c609954136bd9bb1d590f15c2f2b496c37e3d1
chaewon-huh <cw.huh@postech.ac.kr>
2025-03-11 11:39:47 +0000

    chore : change parameters for v0.1.1 train

--- src/train/train_dlora_flux_advanced.sh
+++ src/train/train_dlora_flux_advanced.sh
@@ -1,6 +1,6 @@
 export MODEL_NAME="black-forest-labs/FLUX.1-dev"
-export INSTANCE_DIR="./data/instance-v0.1.0"
-export OUTPUT_DIR="./ckpt/ponix-generator-v0.1.0"
+export INSTANCE_DIR="./data/instance-v0.1.1"
+export OUTPUT_DIR="./ckpt/ponix-generator-v0.1.1"
 
 accelerate launch src/train/train_dlora_flux_advanced.py \
   --pretrained_model_name_or_path "$MODEL_NAME" \
